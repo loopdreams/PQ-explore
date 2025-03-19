30fa03b8b46ceef4c2e201b08e297aa9fc8c5c44
Eoin Carney <eoincarney0@gmail.com>
2025-03-19 14:25:48 +0000

    added hl for llm resp

--- src/notebooks/rag.clj
+++ src/notebooks/rag.clj
@@ -32,11 +32,11 @@
          " Question: " question
          " Context: " ctx)))
 
-(defn ask-llm [question]
+(defn ask-llm [question model]
   (let [prompt (make-prompt question)]
     (-> (client/post "http://localhost:11434/api/chat"
                      {:form-params
-                      {:model "llama3.1"
+                      {:model model
                        :messages [{:role "user" :content prompt}]
                        :stream false}
                       :content-type :json})
@@ -44,17 +44,17 @@
         (json/read-value json/keyword-keys-object-mapper))))
 
 
-(defonce test-response (ask-llm "what is the government doing about about climate change?"))
+(defonce test-response (ask-llm "what is the government doing about about climate change?"  "llama3.1"))
 
 (kind/md
  (:content (:message test-response)))
 
-(defonce test-response-2 (ask-llm "What are the government's plans for legislation?"))
+(defonce test-response-2 (ask-llm "What are the government's plans for legislation?"  "llama3.1"))
 
 (kind/md
  (:content (:message test-response-2)))
 
-(defonce test-response-3 (ask-llm "What measures are the government taking to enhance Ireland's cybersecurity?"))
+(defonce test-response-3 (ask-llm "What measures are the government taking to enhance Ireland's cybersecurity?"  "llama3.1"))
 
 (kind/md
  (:content (:message test-response-3)))
@@ -94,3 +94,95 @@
 (similarity-test "What measures are the government taking to enhance Ireland's cybersecurity?" q/db-store)
 
 (similarity-test (:content (:message test-response-3)) db-store-answers)
+
+;; ## Sentence by Sentence Similarity
+
+(defn split-sentences [text] (remove #(= % "") (str/split text #"\. |\n")))
+
+(split-sentences (-> test-response :message :content))
+
+(defn response-sentence-scores [response]
+  (let [sentences (split-sentences (-> response :message :content))
+        data (mapv (fn [s] (let [score (similarity-test s db-store-answers)]
+                             {:score score
+                              :text s}))
+                   sentences)]
+    data))
+
+
+(defn get-score-color [score]
+  (condp > score
+    0.5 "#F195A9"
+    0.7 "#EAD196"
+    0.8 "#E1EACD"
+    "#BAD8B6"))
+
+
+;; TODO: preserve formatting (newlines, bullets, etc)
+(defn color-response-score [sentence-scores]
+  (into [:p]
+        (map (fn [{:keys [score text]}]
+               [:span {:style (str "background-color: " (get-score-color score))} text])
+             sentence-scores)))
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response)))
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-2)))
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-3)))
+
+;; ## Comparing a question without context
+
+
+(defn ask-llm-no-context [question model]
+  (-> (client/post "http://localhost:11434/api/chat"
+                   {:form-params
+                    {:model model
+                     :messages [{:role "user" :content question}]
+                     :stream false}
+                    :content-type :json})
+      :body
+      (json/read-value json/keyword-keys-object-mapper)))
+
+(defonce test-response-4 (ask-llm-no-context "what is the Irish government doing about about climate change?"  "llama3.1"))
+
+(similarity-test (-> test-response-4 :message :content) db-store-answers)
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-4)))
+
+(defonce test-response-5 (ask-llm-no-context "What measures are the government taking to enhance Ireland's cybersecurity?"  "llama3.1"))
+
+
+(similarity-test (-> test-response-5 :message :content) db-store-answers)
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-5)))
+
+;; Seems like the llm is already quite informed about Ireland's policies!
+;;
+;; ## Comparing Responses from Weaker Models
+;;
+;; Let's try some responses from a 1B parameter model (gemma 3)
+
+
+(defonce test-response-6 (ask-llm "what is the government doing about about climate change?" "gemma3:1b"))
+(defonce test-response-7 (ask-llm-no-context "what is the Irish government doing about about climate change?" "gemma3:1b"))
+
+(kind/md
+ (-> test-response-6 :message :content))
+
+(kind/md
+ (-> test-response-7 :message :content))
+
+(similarity-test (-> test-response-6 :message :content) db-store-answers)
+(similarity-test (-> test-response-7 :message :content) db-store-answers)
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-6)))
+
+(kind/hiccup
+ (color-response-score (response-sentence-scores test-response-7)))
