6072dcfcd66a75328fb3591a4225f06da6d73bfb
Eoin Carney <eoincarney0@gmail.com>
2025-04-26 23:08:42 +0100

    fixed error in retrieval metrics

--- src/notebooks/vdb_evaluation.clj
+++ src/notebooks/vdb_evaluation.clj
@@ -1,26 +1,30 @@
+;; # 3. Retrieval Evaluation
 (ns notebooks.vdb-evaluation
   (:require [clojure.edn :as edn]
             [notebooks.preparation :refer [ds]]
-            [notebooks.vector-database :refer [add-question-to-store! db-store query-db-store]]
+            [notebooks.vector-database :refer [add-question-to-store! db-store-questions query-db-store]]
             [scicloj.kindly.v4.kind :as kind]
             [notebooks.tokenizer :as tokenizer]
             [scicloj.tableplot.v1.plotly :as plotly]
             [clojure.string :as str]
             [tablecloth.api :as tc]
-            [notebooks.vdb-evaluation :as vdb]
-            [notebooks.llm-api :as llm])
+            [notebooks.vdb-evaluation :as vdb])
   (:import
    (dev.langchain4j.data.segment TextSegment)
    (dev.langchain4j.model.openai OpenAiEmbeddingModel)
    (dev.langchain4j.store.embedding.inmemory InMemoryEmbeddingStore)
    (dev.langchain4j.model.embedding.onnx.allminilml6v2 AllMiniLmL6V2EmbeddingModel)))
 
-
-(comment
-  ;; Interesting how many matches there are for this! Shows the duplication of language
-  (filter #(re-find (re-pattern (str/join "|" highlights)) (second %))
-          (map-indexed vector (remove nil? (:answer ds)))))
-
+;; In this section we'll use some standard approaches to try to evaluate the
+;; performance of the vector database.
+;;
+;; The material fetched from the database makes up the context that is provided
+;; in a RAG application, so it is important to try get information that is both
+;; **relevant to the question**, but also **doesn't contain too much superfluous
+;; information** that might confuse the LLM or make the context information
+;; ambiguous.
+;;
+;;
 ;; ## Sample Answers and Questions
 ;;
 ;; For this exercise, we'll first create a small sample of answers
@@ -35,22 +39,17 @@
    "However, local authorities were invited to submit up to 5 applications to the value of €1.5 million per local authority."
    "The salary scale for an archaeologist in the local government sector ranges from €55,519 to €77,176. "])
 
-;; Original Questions
-;; - "Deputy Paul Kehoe asked the Minister for Health to address the provision of GP services in the Navan Road, Pelletstown and Ashtown areas of Dublin West, where people have reported difficulty in accessing GP services and complain that the list of doctors they have been provided with is out of date, including retired and in some cases deceased GPs, and where a medical centre lies empty in Pelletstown; and if he will make a statement on the matter."
-;; - "Deputy Patricia Ryan asked the Minister for Health if his Department plans to put in place core funding for the provision of Level 6 (QQI) courses for healthcare assistants; and if he will make a statement on the matter."
-;; - "Deputy Thomas Gould asked the Minister for Rural and Community Development the role her Department is taking in the National Drugs Strategy."
-;; - "Deputy Donnchadh Ó Laoghaire asked the Minister for Rural and Community Development how her Department will work with local authorities to encourage them to apply for new funding for the town and village renewal scheme; if there will be a minimum submission requirement for each local authority; and if she will make a statement on the matter."
-;; - "Deputy Aengus Ó Snodaigh asked the Minister for Housing, Local Government and Heritage the estimated cost of hiring a qualified archaeologist for every local authority."
-
 (def highlights-questions
   ["What is the government doing to help improve GP services?"
    "Will the government put in place Level 6 (QQI) courses for healthcare assistants?"
    "What is the government doing with regard to the National Drugs Strategy?"
    "How is the government encouraging local authorities to apply for the town and village renewal scheme?"
-   "What is the salary scale for an archaeologiest in the local government sector?"])
+   "What is the salary scale for an archaeologist in the local government sector?"])
 
+;;
+;; ## Evaluating Retrieval
 ;; At the moment, our approach is based on *searching for similar questions*,
-;; and then returning their answers.  However, this is a bit of a 'naive'
+;; and then returning their answers.  However, this is a very naive
 ;; approach. It is based on the concrete steps that are usually taken when trying to answer a new question:
 ;;
 ;; 1. Search for previous similar questions
@@ -59,7 +58,7 @@
 ;;
 ;; Why not just skip this step of searching through questions all together? The
 ;; approach chosen here might depend on the actual application of a system like
-;; this. For example, would the intended use be to help administors prepare
+;; this. For example, would the intended use be to help administers prepare
 ;; answers to new questions? In this case searching through previous questions
 ;; might be more useful. If the intended use is something closer to 'general'
 ;; retrieval of information, then the best approach might be to simply search
@@ -89,146 +88,156 @@
 ;; later on.
 ;;
 ;; To help improve this, let's first take a look at some common, simple metrics
-;; that are used to measure retrieval:
+;; that are used to measure retrieval
+;; ### Retrieval Metrics Overview
 
-;; [Description of the metrics]
+;; #### Recall
+;;
+;; Recall measures how much of the ground truth statement is captured through
+;; the retrieval. It does so by counting how many tokens in the retrieved context
+;; are also present in the ground truth statement.
+;;
+;; Higher recall means that more of the ground truth statement is captured in the context.
+;;
+;; #### Precision
+;; Precision is like the inverse of recall, it counts how many tokens in the
+;; ground-truth statement are present in the retrieved context.
+;;
+;; Higher precision means that there is less 'additional' information captured in the context.
+;;
+;; #### Intersection over Union (IoU)
+;; This is a metric that captures both recall and precision together. It is
+;; calculated by dividing the length of the token overlap by the length of token
+;; union of the retrieved context and the ground truth.
+;;
+;; For example, if we retrieve 200 tokens, the ground truth is 100 tokens, and
+;; the overlap between both is 70 tokens, then the IoU would be 70/(200+100-70)
+;; = 0.304
 
+;; ### Splitting Docs
 
-;; Simply splits into sentences and group sentences by 'chunk size'
-(defn chunking-fn [documents chunk-size]
+;; When thinking about recall/precision, a natural question is around the size
+;; of the documents that are stored in the database. For example, if we are
+;; always retrieving the 5 most similar docs, but each of those are 1000 words
+;; long, then we are always passing the model 5000 words. This will probably
+;; increase the chance of the target information being present, but it will also
+;; increase the amount of superfluous information passed to the LLM (i.e.
+;; decrease precision)
+;;
+;; Therefore, we can try splitting the documents into chunks of various sizes.
+;;
+;; Below is a simple function that does this for us.
+(defn split-document [documents chunk-size]
   (let [sentences (->> (str/split documents tokenizer/sentence-splitter-re)
                        (remove #(= (str/trim %) "")))
-        chunks (->> (partition-all chunk-size sentences)
-                    (mapv #(str/join #" " %)))]
+        chunks    (->> (partition-all chunk-size sentences)
+                       (mapv #(str/join #" " %)))]
     chunks))
 
+;; ### Calculating Metrics for Retrieval Strategies
+;;
+;;
+;; To test our retrieval strategies, we will split up the information contained in the
+;; dataset 'answers' in the following ways:
+;;
+;; - Chunks of 3 sentences (3 Chunks)
+;;
+;; - Chunks of 5 sentences (5 Chunks)
+;;
+;; - Chunks of 10 sentences (10 Chunks)
+;;
+;; - Chunks of 15 sentences (15 Chunks)
+;;
+;; - Original answers, i.e., no splitting (Full Docs)
+;;
+;; We will also test our original 'Question Retrieval' method that we created in
+;; the last section (Question Method).
 
 (def embedding-model (AllMiniLmL6V2EmbeddingModel/new))
 
 (defn chunked-docs [docs chunking-size]
-  (->> (mapv #(chunking-fn % chunking-size) docs) ;; In this case chunk the documents individually, because we know that are all separate/discrete answers
+  (->> (mapv #(split-document % chunking-size) docs) ;; In this case chunk the documents individually, because we know that are all separate/discrete answers
        (remove empty?)
        (reduce into)))
 
+;; To generate metrics, we first retrieve similar documents based on a given
+;; question, then we check those documents against the highlighted (answer)
+;; text.
+;;
+;; Both functions below are mostly identical, except the 'question method'
+;; function adds a step from looking up an answer based on a retrieved similar
+;; question.
 
-;; TODO: add ability to use openai embeddings and test with openai embeddings
-
-
-(def openai-embedding-model (-> (OpenAiEmbeddingModel/builder)
-                                (.apiKey (:openai-api-key (edn/read-string (slurp "secrets.edn"))))
-                                (.modelName "text-embedding-3-small")
-                                (.build)))
-
-
-(defn add-docs-to-store! [doc store embedding-model-name]
-  (if (= embedding-model-name :openai)
-    (let [embedding (->> (. openai-embedding-model embed doc)
-                         (.content))]
-      (. store add embedding (TextSegment/from doc)))
-    (let [segment (TextSegment/from doc)
-          embedding (->> segment (. embedding-model embed) (.content))]
-      (. store add embedding segment))))
-
-
-
-(defn generate-metrics [highlights chunked-docs embedding-m & label]
-  (let [db-store     (InMemoryEmbeddingStore/new)
-        num          (count (mapv #(add-docs-to-store! % db-store embedding-m) chunked-docs))
-        _            (println num)]
-    (loop [[h & hs] highlights
-           results  []]
-      (if-not h
+(defn calculate-metrics [questions answers chunked-docs & label]
+  (let [db-store (InMemoryEmbeddingStore/new)
+        num      (count (mapv #(add-question-to-store! % db-store) chunked-docs))
+        _        (println num)]
+    (loop [idx     (dec (count questions))
+           results []]
+      (if (< idx 0)
         results
-        (let [hl-embedding (if (= embedding-m :openai)
-                             (->> h
-                                  (. openai-embedding-model embed)
-                                  (.content))
-                             (->> (TextSegment/from h)
-                                  (. embedding-model embed)
-                                  (.content)))
-              matches      (->> (. db-store findRelevant hl-embedding 5)
-                                ;; (filterv #(> (.score %) 0.7))
-                                (mapv #(.text (.embedded %)))
-                                (str/join " "))]
-          (recur hs
-                 (conj results (tokenizer/calculate-retrieval-metrics h matches :word (first label)))))))))
+        (let [q-embedding (->> (TextSegment/from (nth questions idx))
+                               (. embedding-model embed)
+                               (.content))
+              matches     (->> (. db-store findRelevant q-embedding 5)
+                               (mapv #(.text (.embedded %)))
+                               (str/join " "))]
+          (recur (dec idx)
+                 (conj results (tokenizer/calculate-retrieval-metrics
+                                (nth answers idx)
+                                matches
+                                :word
+                                (first label)))))))))
 
-
-
-(comment
-  (def test-db-store (InMemoryEmbeddingStore/new))
-  (count (mapv #(add-docs-to-store! % test-db-store :openai) (chunked-docs (take 5 (:answer ds)) 3)))
-  (def test-hl "There are currently 28 bills at various stages across both Houses of the Oireachtas.")
-  (defonce test-hl-emb (->> test-hl
-                            (. openai-embedding-model embed)
-                            (.content)))
-
-  (tokenizer/calculate-retrieval-metrics
-   test-hl
-   (->> (. test-db-store findRelevant test-hl-emb 5)
-        (mapv #(.text (.embedded %)))
-        (str/join " "))
-   :word))
-
-(defn generate-metrics-question-retrieval-method [highlights hl-questions]
+(defn calculate-metrics-question-retrieval-method [hl-answers hl-questions]
   (loop [idx 0
          res []]
-    (if (= idx (count highlights))
+    (if (= idx (count hl-answers))
       res
       (let [q-embedding       (->> (TextSegment/from (nth hl-questions idx))
                                    (. embedding-model embed)
                                    (.content))
-            q-matches         (->> (. db-store findRelevant q-embedding 5)
+            q-matches         (->> (. db-store-questions findRelevant q-embedding 5)
                                    (mapv #(.text (.embedded %))))
             corresponding-ans (->> (tc/select-rows ds #(some #{(:question %)} q-matches))
                                    :answer
                                    (str/join " "))]
-        (recur (inc idx) (conj res (tokenizer/calculate-retrieval-metrics
-                                    (nth highlights idx)
-                                    corresponding-ans
-                                    :word
-                                    "Question Method")))))))
+        (recur (inc idx)
+               (conj res (tokenizer/calculate-retrieval-metrics
+                          (nth hl-answers idx)
+                          corresponding-ans
+                          :word
+                          "Question Method")))))))
 
 
+;; Running these functions over several types of chunking strategies takes a
+;; while, so we are going to run them and save the results to a file.
 (comment
-  (def met-comparisons
-    (let [hls highlights-answers
-          docs (-> ds
-                   (tc/drop-missing :answer)
-                   (tc/drop-rows #(re-find #"details supplied" (% :question)))
-                   (tc/drop-rows #(re-find #"As this is a service matter" (% :answer)))
-                   :answer)
-          full-docs-benchmark (generate-metrics hls docs :langchain "Full Docs")]
+  (defonce metric-comparisons
+    (let [questions           highlights-questions
+          answers             highlights-answers
+          docs                (-> ds
+                                  (tc/drop-missing :answer)
+                                  (tc/drop-rows #(re-find #"details supplied" (% :question))) ;; getting rid of a few extra unhelpful questions/answers
+                                  (tc/drop-rows #(re-find #"As this is a service matter" (% :answer)))
+                                  :answer)
+          full-docs-benchmark (calculate-metrics questions answers docs "Full Docs")]
       (loop [[x & xs] [3 5 10 15]
-             result []]
+             result   []]
         (if-not x
           (conj result full-docs-benchmark)
           (let [chunked-docs (chunked-docs docs x)]
             (recur xs
-                   (conj result (generate-metrics hls chunked-docs :langchain (str x " Chunks")))))))))
+                   (conj result (calculate-metrics questions answers chunked-docs (str "Chunks-" x)))))))))
 
-  (defonce metric-comparisons-openai-embeddings
-    (let [hls highlights-answers
-          docs (-> ds
-                   (tc/drop-missing :answer)
-                   (tc/drop-rows #(re-find #"details supplied" (% :question)))
-                   (tc/drop-rows #(re-find #"As this is a service matter" (% :answer)))
-                   (tc/select-rows (range 1))
-                   :answer)
-          full-docs-benchmark (generate-metrics hls docs :openai "Full Docs")]
-      (loop [[x & xs] [3 5 10 15]
-             result []]
-        (if-not x
-          (conj result full-docs-benchmark)
-          (let [chunked-docs (chunked-docs docs x)]
-            (recur xs
-                   (conj result (generate-metrics hls chunked-docs :openai (str x " Chunks")))))))))
 
-  (spit "data/metrics_retrieval_data.edn" (into (reduce into met-comparisons)
-                                                (generate-metrics-question-retrieval-method highlights-answers highlights-questions)))
-  (spit "data/metrics_retrieval_data_openai.edn" (reduce into met-comparisons-openai-embeddings)))
+  (spit "data/retrieval_metrics/results.edn"
+        (with-out-str (clojure.pprint/pprint
+                       (into (reduce into metric-comparisons)
+                             (calculate-metrics-question-retrieval-method highlights-answers highlights-questions))))))
 
-(def comparison-data (edn/read-string (slurp "data/metrics_retrieval_data.edn")))
+(def comparison-data (edn/read-string (slurp "data/retrieval_metrics/results.edn")))
+
 
 (kind/table comparison-data)
 
@@ -237,9 +246,7 @@
    (/ (apply + coll)
       (count coll))))
 
-
-
-(def temp-ds
+(def ds-metrics-avg
   (->
    (tc/dataset comparison-data)
    (tc/group-by [:label])
@@ -247,20 +254,24 @@
                   :avg-precision #(average (% :precision))
                   :avg-IoU #(average (% :IoU))})))
 
-(-> temp-ds
+
+(-> ds-metrics-avg
     (plotly/layer-line
      {:=x :label
       :=y :avg-recall}))
 
-(-> temp-ds
+
+(-> ds-metrics-avg
     (plotly/layer-line
      {:=x :label
       :=y :avg-precision}))
 
-(-> temp-ds
+
+(-> ds-metrics-avg
     (plotly/layer-line
      {:=x :label
       :=y :avg-IoU}))
+
 
 ;; In the graphs above, the results for 'recall' are very surprising. Generally,
 ;; you would expect recall to go up with longer chunks of text (i.e., there
